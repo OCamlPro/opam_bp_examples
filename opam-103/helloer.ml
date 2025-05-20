eedb29358a1479a9680a8dc523f383e1e8a20363
Raja Boujbel <rjbou@ocamlpro.com>
2025-05-20 14:45:59 +0200

    [opam-103] Add a CLI for helloer

--- opam-103/helloer.ml
+++ opam-103/helloer.ml
@@ -1,4 +1,21 @@
-let hello () =
-  print_endline "Hello OCamlers!!"
+open Cmdliner
 
-let _ =  hello ()
+let hello gentle =
+  print_endline (Helloer_lib.hello gentle)
+
+let gentle =
+  let doc = "Gentle version." in
+  Arg.(value & flag & info ["g"; "gentle"] ~doc)
+
+let cmd =
+  let doc = "print welcome messages" in
+  let man = [
+    `S Manpage.s_bugs;
+    `P "Email bug reports to <b@u.gs>." ]
+  in
+  let info = Cmd.info "helloer" ~version:"-0.1" ~doc ~man in
+  Cmd.v info Term.(const hello $ gentle)
+
+let main () = exit (Cmd.eval cmd)
+
+let _ = main ()
